f8838cafff8162f05fee86c2d0c0cc9199e74247
Simon <simon@sztus.com>
2020-06-17 21:44:12 +0800

    java java java

--- JAVA-BASIC/CompletableFutureVsParallelStream.java
+++ JAVA-BASIC/CompletableFutureVsParallelStream.java
@@ -1,5 +1,5 @@
 
-// link: https://www.jdon.com/idea/java/java-8-completablefuture-vs-parallel-stream.html
+//  link: https://www.jdon.com/idea/java/java-8-completablefuture-vs-parallel-stream.html
 //  https://blog.csdn.net/u014351782/article/details/53812241
 //  https://www.cnblogs.com/taiyonghai/p/9397394.
 //  https://www.cnblogs.com/dennyzhangdd/p/7010972.html
