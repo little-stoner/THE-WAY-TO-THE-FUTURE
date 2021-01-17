352fb41280f8c2008300ac4b4caf99ccd32f04e6
viberring <viberring@gmail.com>
2021-01-17 17:32:27 +0800

    java

--- java/JAVA-BASIC/src/main/java/_0practice/vi/collection/SubList.java
+++ java/JAVA-BASIC/src/main/java/_0practice/vi/collection/SubList.java
@@ -1,2 +1,19 @@
-package _0practice.vi.collection;public class SubList {
+package _0practice.vi.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubList {
+
+    public static void main(String[] args) {
+        List<Integer> a = Arrays.asList(1, 2, 3, 4, 5);
+
+        System.out.println(a.subList(0, 0));
+        System.out.println(a.subList(0, 2));
+
+        a.subList(0, 5).set(2, 9);
+        System.out.println(a);
+
+    }
+
 }
