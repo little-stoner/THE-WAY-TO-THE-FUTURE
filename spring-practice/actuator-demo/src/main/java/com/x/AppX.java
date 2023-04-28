9d33500be6a68df518bee2cdbf8f4821a1a73c7a
viber <33152725+Viberring@users.noreply.github.com>
2023-04-28 14:19:17 +0800

    spring

--- spring-practice/actuator-demo/src/main/java/com/x/AppX.java
+++ spring-practice/actuator-demo/src/main/java/com/x/AppX.java
@@ -1,5 +1,13 @@
+package com.x;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class AppX {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppX.class, args);
+    }
+
 }
