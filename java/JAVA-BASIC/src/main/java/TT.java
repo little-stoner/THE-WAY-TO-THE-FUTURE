2cddeed9d6b160cdc3348c05be85f873d6cd9bdf
viber <33152725+Viberring@users.noreply.github.com>
2023-05-15 15:59:58 +0800

    clear some file

--- java/JAVA-BASIC/src/main/java/TT.java
+++ java/JAVA-BASIC/src/main/java/TT.java
@@ -1,10 +1,14 @@
+import com.alibaba.fastjson.JSONObject;
+
 import java.math.BigDecimal;
 
 public class TT {
 
+    private static class A {}
 
     public static void main(String[] args) {
-
+        A a = JSONObject.parseObject("", A.class);
+        System.out.println(a);
     }
 }
 
